ed66eafea501051081c76ad05038b10dedef84d0
Aritan Huang <aritan@AritandeMacBook-Air.local>
2022-09-21 14:47:39 +0800

    modify [sidebar 點擊後加上active]

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,13 +2,18 @@
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 //JQuery
-document.documentElement.style.setProperty('--vh', `${vh}px`);
 $(document).ready(function () {
     //收合側欄選單寫法
     $('.btn-expand').click(function (e) {
         e.preventDefault();
         $('body').toggleClass('expand');
+    });
+    //按著選單後加上樣式
+    $('.menu li').click(function (e) {
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active');
     });
 });
