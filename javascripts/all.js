4e7f653d51828d2330e1eac7769dab086070f5e3
Aritan Huang <aritan@AritandeMacBook-Air.local>
2022-09-21 13:12:34 +0800

    modify [sidebar 登出按鈕mobile裝置位置JS版]

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,3 +1,10 @@
+//解決mobile無法滿高問題
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+
+//JQuery
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 $(document).ready(function () {
     //收合側欄選單寫法
     $('.btn-expand').click(function (e) {
